0144dc3767786497bbc423ac7d7a52354f56591b
anton-mladenov <tonkata@mail.com>
2018-08-12 20:36:35 +0200

    DB can connect to server now

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata'
+import {createKoaServer, Action} from 'routing-controllers'
+// import PageController from './pages/controller'
+import setupDb from './db'
+import { verify } from './jwt';
+
+const app = createKoaServer({
+  controllers: [
+
+  ],
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+        const [ , token ] = header.split(' ')
+        return !!(token && verify(token))
+      }
+    return false
+}
+})
+
+
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
